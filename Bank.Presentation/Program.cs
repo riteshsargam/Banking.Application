f7617158ca61eb355af9e3eb6064ae5099d24874
Ritesh <ritesh.sargam00@gmail.com>
2025-02-20 23:57:27 +0530

    Creating Login Form

--- Bank.Presentation/Program.cs
+++ Bank.Presentation/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        //display title
+        Console.WriteLine("******Banking Application***");
+        Console.WriteLine("--Login Page--");
+
+        //declare variables to store username and password;
+        string userName = null, password = null;
+
+        //read userName from keyboard
+        Console.Write("Username:");
+        userName = Console.ReadLine();
+
+        //read password from keyboard
+        Console.Write("Password:");
+        password = Console.ReadLine();
+
+        //check username and password
+        if (userName == "system" && password == "manager")
+        {
+            Console.WriteLine("TO DO: Main menu here");
+        }
+
+        //about to exit
+        Console.WriteLine("Thank you! Visit again");
+        Console.ReadLine();
+    }
+}
