ebfadd4f2bd037e14db719540f77a4295b295cb5
Ritesh <ritesh.sargam00@gmail.com>
2025-02-21 00:21:15 +0530

    2. Creating Main Menu

--- Bank.Presentation/Program.cs
+++ Bank.Presentation/Program.cs
@@ -20,7 +20,38 @@
         //check username and password
         if (userName == "system" && password == "manager")
         {
-            Console.WriteLine("TO DO: Main menu here");
+            //declare variable to store menu choice
+            int mainMenuChoice = -1;
+
+            do
+            {
+                //show main menu
+                Console.WriteLine("Main menu");
+                Console.WriteLine("1.Customer");
+                Console.WriteLine("2.Account");
+                Console.WriteLine("3.Funds Transfer");
+                Console.WriteLine("4.Funds Transfer Statement");
+                Console.WriteLine("5.Account Statement");
+                Console.WriteLine("0.Exit");
+
+                //accept menu choice from keyboard
+                Console.WriteLine("Enter choice:");
+                mainMenuChoice = int.Parse(Console.ReadLine());
+
+                switch (mainMenuChoice)
+                {
+                    case 1: //TO DO: Display Customer Menu
+                        break;
+                    case 2: //TO DO: Display Account Menu
+                        break;
+                    case 3: //TO DO: Display Funds Transfer Menu
+                        break;
+                    case 4: //TO DO: Display Funds Transfer Statement Menu
+                        break;
+                    case 5: //TO DO: Display Account Statement Menu
+                        break;
+                } 
+            } while (mainMenuChoice != 0);
         }
 
         //about to exit
